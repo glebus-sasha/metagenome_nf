c1638a7f1267bbf51c5d8908ceb8f2763467016b
glebus-sasha <glebus-sasha@mail.ru>
2024-11-26 21:14:22 +0300

    fix: config

---
+++ path_config.py
@@ -0,0 +1,4 @@
+params.outdir           = "/storage/aglebus/meta/results"                                           // Output directory for results
+params.reads            = "/storage/aglebus/meta/data"                                              // Reads files
+params.kraken2_db       = "/storage/aglebus/meta/kraken2_db"                                        // Data base for kraken2
+params.gtdbtk_db        = "/storage/aglebus/meta/GTDB_db/release220"                                // Data base for GTDB-TK
