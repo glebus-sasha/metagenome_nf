367e368f88253e161d14e40407a343adf4e66f3d
glebus-sasha <glebus-sasha@mail.ru>
2024-11-26 21:24:43 +0300

    fix: path config

--- path_config.py
+++ path_config.py
@@ -2,6 +2,6 @@ READS_FOLDER = "/home/aglebus/Documents/metagenome_nf/reads"
 OUTPUT_FOLDER = "/home/aglebus/Documents/metagenome_nf/output"
 nextflow_path = "/home/aglebus/Documents/metagenome_nf"
 kraken2_db       = "/storage/aglebus/meta/kraken2_db"
-gtdbtk_db        = "/storage/aglebus/meta/GTDB_db/release220"
+GTDB_db        = "/storage/aglebus/meta/GTDB_db/release220"
 
 nextflow_command = ["nextflow", "run", "./main.nf", "-profile", "singularity", "--reads", READS_FOLDER, "--outdir", OUTPUT_FOLDER, "--kraken2_db", kraken2_db, "--gtdbtk_db", GTDB_db]
